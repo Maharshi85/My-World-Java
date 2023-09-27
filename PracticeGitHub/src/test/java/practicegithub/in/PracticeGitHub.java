ee86dacd2edbb636676759478f975344c7e0e267
HP <HP@DESKTOP-CVD89PB>
2023-09-27 11:25:04 +0530

    SecondCommit

--- PracticeGitHub/src/test/java/practicegithub/in/PracticeGitHub.java
+++ PracticeGitHub/src/test/java/practicegithub/in/PracticeGitHub.java
@@ -4,5 +4,9 @@ public class PracticeGitHub {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
 	}
 }
